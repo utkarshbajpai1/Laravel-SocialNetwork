aa3cc8c94f2826c31e31509ee1d3224d6c219f66
utkarsh_bajpai <ubajpai1aa@gmail.com>
2017-08-06 17:39:09 +0530

    templating formats
    templating formats

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title> Laravel-Social Network </title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  </head>
-  <body>
-      <h1> Welcome to social network </h1>
-        <p> this is home page </p>
-  </body>
-</html>
+@extends('templates.default')
+
+@section('title', '| home')
+
+@section('content')
+  <h1> Welcome to the social network </h1>
+@stop
